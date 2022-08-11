c23a6d5892b648e57a7606abcfe7d664c9b9bf59
Julissa Gazca <julissa.gazca@gmail.com>
2022-08-10 17:10:03 -0700

    updates to gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,9 +29,9 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
         // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
       },
     },
     `gatsby-plugin-react-helmet`,
